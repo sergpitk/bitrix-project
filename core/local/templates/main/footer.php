fdfb311242bfc980df77632b35e36ef6e7947f2c
v.kryukov <v.kryukov@regiomedia.ru>
2016-08-21 23:35:58 +0300

    Базовые-пребазовые файлы шаблона и начальный игнор

---
+++ core/local/templates/main/footer.php
@@ -0,0 +1,11 @@
+
+        </section>
+    </main>
+</div>
+<div class="page__bottom">
+    <footer class="footer">
+        <div class="footer__inner">
+            
+        </div>
+    </footer>
+</div>
