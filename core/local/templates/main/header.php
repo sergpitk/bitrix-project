d227f4a75651ed4c0eba8bf815bfa1f31736b94e
v.kryukov <v.kryukov@regiomedia.ru>
2016-08-31 23:44:05 +0300

    Исправил ошибки и предупреждения линтера

--- core/local/templates/main/header.php
+++ core/local/templates/main/header.php
@@ -8,7 +8,7 @@
     <?php $APPLICATION->AddHeadScript(SITE_TEMPLATE_PATH.'/scripts.js')?>
 
 </head>
-<body class="page page_<?=LANGUAGE_ID?> page_<?$APPLICATION->ShowProperty('page_type', 'secondary')?>">
+<body class="page page_<?=LANGUAGE_ID?> page_<?php $APPLICATION->ShowProperty('page_type', 'secondary')?>">
 <?php $APPLICATION->ShowPanel()?>
 
 <div class="page__top">
@@ -19,7 +19,7 @@
     </header>
 
     <main class="page__middle">
-        <section class="page__content" style="background-image: url('<?$APPLICATION->ShowProperty('page_image')?>')">
-            <?php if ($APPLICATION->GetCurPage(false) != SITE_DIR):?>
+        <section class="page__content" style="background-image: url('<?php $APPLICATION->ShowProperty('page_image')?>')">
+            <?php if ($APPLICATION->GetCurPage(false) != SITE_DIR) :?>
                 <h1 class="page__title"><?php $APPLICATION->ShowTitle(false)?></h1>
             <?php endif;?>
--- deploy.php
+++ deploy.php
@@ -3,11 +3,11 @@ use Symfony\Component\Yaml\Yaml;
 
 require 'vendor/autoload.php';
 require 'recipe/common.php'; // assumes deployer installed globally
-require 'vendor/deployphp/recipes/recipes/configure.php'; 
+require 'vendor/deployphp/recipes/recipes/configure.php';
 
 // Set deployment configuration
 set('repository', '');
-set('writable_dirs', []); 
+set('writable_dirs', []);
 set('clear_paths', []);
 set('clear_use_sudo', false);
 set('keep_releases', 5);
@@ -20,57 +20,67 @@ set('sync_dirs', ['bitrix', 'upload']);
 option('source-server', null, \Symfony\Component\Console\Input\InputOption::VALUE_OPTIONAL, 'Server to get data from.');
 option('dest-path', null, \Symfony\Component\Console\Input\InputOption::VALUE_OPTIONAL, 'Path to save sync directories to.');
 
-task("sync:db", function(){
+task(
+    "sync:db",
+    function () {
 
-	$serverList = $serverInfo = Yaml::parse(file_get_contents(__DIR__.'/stage/servers.yml'));
+        $serverList = $serverInfo = Yaml::parse(file_get_contents(__DIR__.'/stage/servers.yml'));
 
-	$sourceServerKey = null;
-	if (input()->hasOption('source-server')) {
-        $sourceServerKey = input()->getOption('source-server');
-    }
-
-    $sourceServer = $serverList[$sourceServerKey];
-    $destServer = $serverList[env('server.name')];
-
-	runLocally("ssh {$sourceServer['user']}@{$sourceServer['host']} 'mysqldump -h {$sourceServer['app']['mysql']['host']} -u {$sourceServer['app']['mysql']['username']} -p{$sourceServer['app']['mysql']['password']} {$sourceServer['app']['mysql']['dbname']}  --skip-lock-tables --add-drop-table' | mysql -h {$destServer['app']['mysql']['host']} -u{$destServer['app']['mysql']['username']}  -p{$destServer['app']['mysql']['password']} {$destServer['app']['mysql']['dbname']}", 0);
-
-})->onlyOn('dev');
-
-task("sync:dirs", function(){
-
-	$serverList = $serverInfo = Yaml::parse(file_get_contents(__DIR__.'/stage/servers.yml'));
-
-	$sourceServerKey = null;
-	if (input()->hasOption('source-server')) {
-        $sourceServerKey = input()->getOption('source-server');
-    }
-
-    $destPath = null;
-    if (input()->hasOption('dest-path')) {
-        $destPath = input()->getOption('dest-path');
-    }
-	$sourceServer = $serverList[$sourceServerKey];
-    $destServer = $serverList[env('server.name')];
-
-
-    foreach (get('sync_dirs') as $dir) {
-        $excludeString = '';
-        foreach (get('sync_excludes') as $exclude) {
-            $excludeString .= " --exclude=$exclude ";
+        $sourceServerKey = null;
+        if (input()->hasOption('source-server')) {
+            $sourceServerKey = input()->getOption('source-server');
         }
 
-        writeln("rsync -avz --delete {$sourceServer['user']}@${sourceServer['host']}:${sourceServer['webroot']}/$dir/ $destPath/$dir $excludeString");
+        $sourceServer = $serverList[$sourceServerKey];
+        $destServer = $serverList[env('server.name')];
 
-        runLocally("rsync -avz --delete {$sourceServer['user']}@${sourceServer['host']}:${sourceServer['webroot']}/$dir/ $destPath/$dir $excludeString", 0);
+        runLocally("ssh {$sourceServer['user']}@{$sourceServer['host']} 'mysqldump -h {$sourceServer['app']['mysql']['host']} -u {$sourceServer['app']['mysql']['username']} -p{$sourceServer['app']['mysql']['password']} {$sourceServer['app']['mysql']['dbname']}  --skip-lock-tables --add-drop-table' | mysql -h {$destServer['app']['mysql']['host']} -u{$destServer['app']['mysql']['username']}  -p{$destServer['app']['mysql']['password']} {$destServer['app']['mysql']['dbname']}", 0);
     }
-});
+)->onlyOn('dev');
 
-task('sync', [
-	'sync:dirs',
-	'sync:db'
-]); 
+task(
+    "sync:dirs",
+    function () {
 
-task('deploy', [
+        $serverList = $serverInfo = Yaml::parse(file_get_contents(__DIR__.'/stage/servers.yml'));
+
+        $sourceServerKey = null;
+        if (input()->hasOption('source-server')) {
+            $sourceServerKey = input()->getOption('source-server');
+        }
+
+        $destPath = null;
+        if (input()->hasOption('dest-path')) {
+            $destPath = input()->getOption('dest-path');
+        }
+        $sourceServer = $serverList[$sourceServerKey];
+        $destServer = $serverList[env('server.name')];
+
+
+        foreach (get('sync_dirs') as $dir) {
+            $excludeString = '';
+            foreach (get('sync_excludes') as $exclude) {
+                $excludeString .= " --exclude=$exclude ";
+            }
+
+            writeln("rsync -avz --delete {$sourceServer['user']}@${sourceServer['host']}:${sourceServer['webroot']}/$dir/ $destPath/$dir $excludeString");
+
+            runLocally("rsync -avz --delete {$sourceServer['user']}@${sourceServer['host']}:${sourceServer['webroot']}/$dir/ $destPath/$dir $excludeString", 0);
+        }
+    }
+);
+
+task(
+    'sync',
+    [
+    'sync:dirs',
+    'sync:db'
+    ]
+);
+
+task(
+    'deploy',
+    [
     'deploy:prepare',
     'deploy:release',
     'deploy:update_code',
@@ -80,7 +90,8 @@ task('deploy', [
     'deploy:symlink',
     'deploy:clean',
     'cleanup',
-])->desc('Deploy your project');
+    ]
+)->desc('Deploy your project');
 after('deploy', 'success');
 
 serverList(__DIR__.'/stage/servers.yml');
