bbb32622e93a3b4aac5d6ea42b998804ba86f274
V <V@V.ru>
2017-01-16 23:04:16 +0300

    Добавлена обработка изображений и спрайтов с помощью соответствующих postcss-плагинов

--- gulp/styles.js
+++ gulp/styles.js
@@ -1,14 +1,43 @@
+import path from 'path';
+import through from 'through2';
 import gulp from 'gulp';
 import rename from 'gulp-rename';
 import postcss from 'gulp-postcss';
 import postcssNested from 'postcss-nested';
 import postcssImport from 'postcss-import';
+import postcssAssets from 'postcss-assets';
+import postcssSprites from 'postcss-sprites';
 import autoprefixer from 'autoprefixer';
 
+let spritesCfg = {
+    spritePath: null,
+    stylesheetPath: null,
+    filterBy: function(image) {
+        if (image.url.indexOf('images/sprite') === -1) {
+            return Promise.reject();
+        }
+        return Promise.resolve();
+    },
+    groupBy: function(image) {
+        return Promise.resolve(path.basename(image.styleFilePath));
+    }
+}
 
 gulp.task('styles', () => (
         gulp.src('core/local/**/assets-raw/styles/*.css', {base: './'})
-            .pipe(postcss([postcssNested, postcssImport, autoprefixer()]))
+            .pipe(through.obj(function (file, enc, cb) {
+                spritesCfg.stylesheetPath = path.dirname(file.path).replace('assets-raw', 'assets-done');
+                spritesCfg.spritePath = spritesCfg.stylesheetPath.replace('styles', 'images');
+                cb(null, file);
+            }))
+            .pipe(postcss([
+                postcssNested,
+                postcssImport,
+                postcssAssets,
+                postcssSprites(spritesCfg),
+                autoprefixer()
+                ]
+            ))
             .pipe(rename(function (path) {
                 path.dirname = path.dirname.replace('assets-raw', 'assets-done')
             }))
