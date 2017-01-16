8248cdca5842cc02654b9ca84eccb75b84cb5b59
V <V@V.ru>
2017-01-16 23:51:00 +0300

    Добавлена задача по оптимизации изображений

---
+++ gulp/images.js
@@ -0,0 +1,16 @@
+import gulp from 'gulp';
+import rename from 'gulp-rename';
+import imagemin from 'gulp-imagemin';
+
+
+gulp.task('images', () => {
+    gulp.src([
+            'core/local/**/assets-raw/images/**',
+            '!**/images/sprite/*'
+        ], {base: './'})
+        .pipe(imagemin())
+        .pipe(rename(function (path) {
+            path.dirname = path.dirname.replace('assets-raw', 'assets-done')
+        }))
+        .pipe(gulp.dest('./'))
+});
